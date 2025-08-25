3ba2076b62b63137e494100b13f5036573853110
SaurabhT91 <saurabhtidgam1991@gmail.com>
2025-08-25 22:32:22 +0530

    Update document.py

--- app/models/document.py
+++ app/models/document.py
@@ -2,6 +2,7 @@ from typing import Optional
 from datetime import date
 from sqlmodel import SQLModel, Field
 
+
 class Document(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
